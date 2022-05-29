9e79e7272384fa0a6d393854042a66f04d8f5d6b
AnggaGemilang <anggagemilang12@gmail.com>
2022-05-30 05:46:12 +0700

    splash screen done

--- app/src/main/java/com/example/uasppb/SplashScreenActivity.java
+++ app/src/main/java/com/example/uasppb/SplashScreenActivity.java
@@ -1,20 +1,48 @@
 package com.example.uasppb;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.example.uasppb.databinding.ActivitySplashScreenBinding;
 
+@SuppressLint("CustomSplashScreen")
 public class SplashScreenActivity extends AppCompatActivity {
 
     private ActivitySplashScreenBinding binding;
+    private Integer progressStatus = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivitySplashScreenBinding.inflate(getLayoutInflater());
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(binding.getRoot());
-    }
 
+        binding.progressBar.setMax(100);
+
+        new Thread(new Runnable() {
+            @RequiresApi(api = Build.VERSION_CODES.P)
+            @Override
+            public void run() {
+                while (progressStatus < binding.progressBar.getMax()) {
+                    progressStatus++;
+                    binding.progressBar.setProgress(progressStatus);
+                    try {
+                        Thread.sleep(30);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        }).start();
+    }
 }
