5539f7ec6c6dcf016ee04d5d9972352e71180a45
AnggaGemilang <anggagemilang12@gmail.com>
2022-05-30 13:26:56 +0700

    conflict

--- app/src/main/java/com/example/uasppb/MainActivity.java
+++ app/src/main/java/com/example/uasppb/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.uasppb;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.uasppb.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -67,6 +71,31 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         binding.getRoot().requestFocus();
         makeCurrentFragment(new DashboardFragment());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.item_menu_actionbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case (R.id.act_filter):
+                Toast.makeText(this, "Modal Muncul", Toast.LENGTH_SHORT).show();
+                break;
+            case  (R.id.act_about):
+                new AlertDialog.Builder(this)
+                        .setTitle("Versi Aplikasi")
+                        .setMessage("Beta 1.0.0")
+                        .setCancelable(true)
+                        .setPositiveButton("OK", null)
+                        .create()
+                        .show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
